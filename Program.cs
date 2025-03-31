b8e0ea2444fbcb3e4ddf9b2806776d557de73acc
Gabriel <lab53@fatec.com>
2025-03-31 08:18:10 -0300

    Programa FInalizado

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿double milha, km = 0;
+
+
+Console.WriteLine("Conversor de Milhas para Km");
+
+Console.Write("Digite uma medida em milhas: ");
+milha = double.Parse(Console.ReadLine()!);
+
+km = milha * 1.609;
+
+Console.Write($"O valor em  quilometros é: {km}");
+
+
+
+
